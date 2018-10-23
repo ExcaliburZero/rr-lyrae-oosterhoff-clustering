b80bf987fcf22483d85fd281ed5f53f24a1d469d
Christopher Wells <cwellsny@nycap.rr.com>
2018-10-22 20:41:40 -0400

    Add script to summaries light curve observation counts

---
+++ src/tools/light_curve_observation_stats.py
@@ -0,0 +1,39 @@
+import os
+import os.path
+
+import pandas as pd
+
+def main():
+    print("LMC V band")
+    print("----------")
+    print_light_curve_stats("../../data/raw/lmc/curves/V")
+
+    print("LMC I band")
+    print("----------")
+    print_light_curve_stats("../../data/raw/lmc/curves/I")
+
+    print("SMC V band")
+    print("----------")
+    print_light_curve_stats("../../data/raw/smc/curves/V")
+
+    print("SMC I band")
+    print("----------")
+    print_light_curve_stats("../../data/raw/smc/curves/I")
+
+def print_light_curve_stats(lc_dir):
+    observation_counts = []
+
+    for filename in os.listdir(lc_dir):
+        if filename.endswith(".csv"):
+            # Read in the light curve
+            curve_file = os.path.join(lc_dir, filename)
+            curve = pd.read_csv(curve_file)
+
+            # Record the number of observations
+            count = len(curve)
+            observation_counts.append(count)
+
+    print(pd.Series(observation_counts).describe())
+
+if __name__ == "__main__":
+    main()
